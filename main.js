4dfc0c37eaebbf9ea56134dc5aecb48122281d93
bramos33 <bramosd@yahoo.com>
2020-04-13 15:19:21 -0400

    working on adding rows of songs to the table

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow}  = require('electron')
+const { app, BrowserWindow } = require('electron')
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   let win = new BrowserWindow({
     width: 800,
@@ -9,10 +9,19 @@ function createWindow () {
     webPreferences: {
       nodeIntegration: true
     }
+    
   })
+  win.webContents.openDevTools()
+
+  
+
+
+
 
   // and load the index.html of the app.
   win.loadFile('index.html')
 }
+
+
 
 app.whenReady().then(createWindow)
