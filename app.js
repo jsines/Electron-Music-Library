861d5cea20296562ca82f527ae9ad3e43c70e0c9
bramos33 <bramosd@yahoo.com>
2020-04-13 17:52:31 -0400

    got the table to display

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ console.log(songTable)
 var songs = [['song1', 'artist1', 'album1'], ['song2', 'artist2', 'album2'], ['song3', 'artist3', 'album3']]
 
 function appendSongs(rows) {
-    const songRow
-    const songElement
+    var songRow
+    var songElement
     rows.forEach(things => {
         songRow = songTable.insertRow(-1)
         things.forEach(element => {
