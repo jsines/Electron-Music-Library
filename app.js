cf6a53da60865d3f2c7ca82af58cc7366f91b811
bramos33 <bramosd@yahoo.com>
2020-04-13 17:48:20 -0400

    created app.java to output songs

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const songTable = document.getElementById('songTable')
+
+var songTable = document.getElementById('songTable')
+console.log(songTable)
+
+
+
 
 var songs = [['song1', 'artist1', 'album1'], ['song2', 'artist2', 'album2'], ['song3', 'artist3', 'album3']]
 
 function appendSongs(rows) {
-    const songRow = document.createElement('tr')
-    const songElement = document.createElement('td')
+    const songRow
+    const songElement
     rows.forEach(things => {
+        songRow = songTable.insertRow(-1)
         things.forEach(element => {
-            songElement.innerText = element
-            songRow.append(songElement)
+            
+            songElement = songRow.insertCell(-1)
+            songElement.innerHTML = element
         })
-        songTable.append(songRow)
+        
     })
+
+
+
 }
 
 appendSongs(songs)
