c39421c1b9699918c9f9477b907ee26bc211baf2
Mallo <jsines99@gmail.com>
2020-04-16 03:29:39 -0400

    Play buttons that work!

--- app.js
+++ app.js
@@ -37,19 +37,28 @@ function GetSongsForDisplay(mode, callback) {
     var songName;
     var albumName;
     var artistName;
+    var fileName;
+    var albumCover;
     var songs = []
-    connection.query('SELECT song_name, album_name, artist_name FROM song s INNER JOIN album al ON s.album_id = al.album_id INNER JOIN artist ar ON ar.artist_id = al.artist_id ORDER BY ' + sortBy, function (err, result, fields) {
+    connection.query('SELECT song_name, album_name, artist_name, song_file, album_cover FROM song s INNER JOIN album al ON s.album_id = al.album_id INNER JOIN artist ar ON ar.artist_id = al.artist_id ORDER BY ' + sortBy, function (err, result, fields) {
         if (err) throw err;
         Object.keys(result).forEach(function (key) {
             var row = result[key];
             songName = row.song_name;
             albumName = row.album_name;
             artistName = row.artist_name;
-            var song = new Song(songName, artistName, albumName)
+            fileName = row.song_file;
+            albumCover = row.album_cover;
+            var song = new Song(songName, artistName, albumName, fileName, albumCover)
             songs.push(song)
         })
         return callback(songs)
     })
+}
+
+function PlaySong(songFile, coverFile){
+	changeNowPlaying(songFile)
+	changeAlbumArtwork(coverFile)
 }
 
 function DisplaySongsInTable(songs){
@@ -59,24 +68,30 @@ function DisplaySongsInTable(songs){
     for (const value of songValues){
         songRow = songTable.insertRow(-1)
         Object.keys(value).forEach(function (key) {
-            songData = songRow.insertCell(-1)
-            songData.innerHTML = value[key]
+        	if(key != "songFile" && key != "albumCover"){
+        		console.log(key)
+            	songData = songRow.insertCell(-1)
+            	songData.innerHTML = value[key]
+            }
         })
+        songPlay = songRow.insertCell(-1)
+        console.log(value["albumCover"])
+        songPlay.innerHTML = "<button type='button' onClick='PlaySong(\"" + value["songFile"] + "\", \"" + value["albumCover"] + "\")'>Play</button>"
     }
 }
 
 class Song {
-    constructor(songName, songArtist, songAlbum) {
-
+    constructor(songName, songArtist, songAlbum, songFile, albumCover) {
         this.songName = songName;
         this.songArtist = songArtist;
         this.songAlbum = songAlbum;
-
+        this.songFile = songFile;
+        this.albumCover = albumCover;
     }
 }
 
 function GetSongsHelper(){
-	songTable.innerHTML = "<tr><th>Song</th><th>Artist</th><th>Album</th></tr>"
+	songTable.innerHTML = "<tr><th>Song</th><th>Artist</th><th>Album</th><th>Play</th></tr>"
 	let mode;
 	if(document.getElementById("song_radio").checked){
 		mode = 0;
